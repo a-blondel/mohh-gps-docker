330c0f7565e1005b9348ba5f6c285890b97536a9
Jolan GORGES <jolan.gorges@outlook.com>
2024-11-06 21:23:57 +0100

    fix wine error related to window creation

--- start_gps_instances.sh
+++ start_gps_instances.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$GPS_NAME" ] || [ -z "$GPS_PWD" ] || [ -z "$GPS_ADM_PWD" ]; then
-    echo "One or more mandatory environment variables are not set" >> /var/log/mohh-gps/mohz.log
+    echo "One or more mandatory environment variables are not set"
     exit 1
 fi
 
@@ -16,16 +16,16 @@ GPS_PORT=$((GPS_PORT - 1))
 start_instance() {
     sleep 5 # wait for the previous instance to start
 
-    export WINEPREFIX=/root/.wine
-    export WINE=/opt/lutris-GE-Proton8-26-x86_64/bin/wine
-    export DISPLAY=
-
     local port=$1
-    echo "============================================================" >> /var/log/mohh-gps/mohz.log
-    echo "$(date): Starting instance on port $port" >> /var/log/mohh-gps/mohz.log
-    echo "============================================================" >> /var/log/mohh-gps/mohz.log
-    $WINE /var/www/mohh-gps/mohz.exe -name:$GPS_NAME -pwd:$GPS_PWD -port:$port -adminpwd:$GPS_ADM_PWD -easerver >> /var/log/mohh-gps/mohz.log 2>&1 &
+    echo "============================================================"
+    echo "$(date): Starting instance on port $port"
+    echo "============================================================"
+
+    wine /var/www/mohh-gps/mohz.exe -name:$GPS_NAME -pwd:$GPS_PWD -port:$port -adminpwd:$GPS_ADM_PWD -easerver &
 }
+
+Xvfb :0 -screen 0 1024x768x16 &
+winecfg > /dev/null 2>&1
 
 for i in $(seq 1 $GPS_INSTANCES); do
     port=$(($GPS_PORT + i))
